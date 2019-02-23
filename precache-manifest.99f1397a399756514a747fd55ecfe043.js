8464c38dca091d7c7bbd3092c9536668bb949b0d
miyurumadu <miyurumadusanka@yahoo.com>
2019-02-24 01:55:54 +0530

    Updates

---
+++ precache-manifest.99f1397a399756514a747fd55ecfe043.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9f12b38335ac798d69a",
+    "url": "/js-with-me/react/static/js/runtime~main.f9f12b38.js"
+  },
+  {
+    "revision": "c1e514db5638e9bec4d6",
+    "url": "/js-with-me/react/static/js/main.c1e514db.chunk.js"
+  },
+  {
+    "revision": "fc042165d05bac87347c",
+    "url": "/js-with-me/react/static/js/1.fc042165.chunk.js"
+  },
+  {
+    "revision": "c1e514db5638e9bec4d6",
+    "url": "/js-with-me/react/static/css/main.4fb9919f.chunk.css"
+  },
+  {
+    "revision": "415c1d36cd5f86c23d9f40c4a97e6e58",
+    "url": "/js-with-me/react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/js-with-me/precache-manifest.db943081f42e039a958447e09c80279a.js"
+  "/js-with-me/react/precache-manifest.99f1397a399756514a747fd55ecfe043.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/js-with-me/index.html", {
+workbox.routing.registerNavigationRoute("/js-with-me/react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
